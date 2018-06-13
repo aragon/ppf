6aab7fa5d4ad0f256dad5ee2a6108261b72b38ce
Jorge Izquierdo <jorge@izqui.me>
2018-06-13 20:49:03 +0200

    ppf-server: implement update many rates in website

--- packages/ppf-server/web/index.js
+++ packages/ppf-server/web/index.js
@@ -10,7 +10,23 @@ module.exports = requestData => {
   })
 
   router.get('/rates', (req, res, next) => {
-    res.send(requestData(req.env))
+    let data = requestData(req.env)
+
+    const bases = data.map(p => p.baseToken)
+    const quotes = data.map(p => p.quoteToken)
+    const prices = data.map(p => p.price)
+    const whens = data.map(p => p.when)
+    const sigs = data.map(p => p.sig)
+    
+    const calldata = PPF.encodeManyCall(bases, quotes, prices, whens, sigs)
+
+    data.forEach(({ when }, i) => data[i].date = new Date(1000 * when))
+
+    res.render('rates', {
+      ppf: req.query.ppf || '0x',
+      calldata,
+      rates: data,
+    })
   })
   
   router.get('/rate/:base/:quote', (req, res, next) => {
