5a8567e1af0e2680fb2736303800a102267dcfa0
Jorge Izquierdo <jorge@izqui.me>
2018-06-14 19:38:30 +0200

    ppf.js fix: 18 decimals always

--- packages/ppf.js/index.js
+++ packages/ppf.js/index.js
@@ -9,7 +9,7 @@ const updateManyFunction = ppfABI.find(x => x.name == 'updateMany' && x.type == 
 
 const HASH_ID = soliditySha3({ t: 'string', v: 'PPF-v1' })
 const ONE = new BigNumber(10).pow(18)
-const formatRate = n => new BigNumber(n.toFixed(20)).times(ONE)
+const formatRate = n => new BigNumber(n.toFixed(18)).times(ONE)
 const parseRate = x => x.div(ONE).toNumber().toFixed(4)
 
 const updateDataHash = (base, quote, xrt, when) => {
