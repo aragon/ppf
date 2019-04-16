52c2b216714faa2c6e0413fe9d38c1e1e6e30204
Jorge Izquierdo <izqui97@gmail.com>
2019-04-16 09:29:41 +0200

    chore: ppf deployment script (#18)

--- packages/ppf-contracts/truffle.js
+++ packages/ppf-contracts/truffle.js
@@ -1,16 +1,1 @@
-module.exports = {
-	networks: {
-		rpc: {
-			host: 'localhost',
-			port: 8545,
-			network_id: "*"
-		},
-		coverage: {
-			host: "localhost",
-			network_id: "*",
-			port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-			gas: 0xfffffffffff, // <-- Use this high gas value
-			gasPrice: 0x01      // <-- Use this low gas price
-		},
-	}
-};
+module.exports = require('@aragon/os/truffle-config')
