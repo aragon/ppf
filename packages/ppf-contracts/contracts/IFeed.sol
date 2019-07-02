c989477c374428df10a7cb603422bd79f3862d2f
Facundo Spagnuolo <facundo_spagnuolo@icloud.com>
2019-07-02 20:18:37 -0300

    ppf-contracts: add exchange rate precision function to `IFeed`

--- packages/ppf-contracts/contracts/IFeed.sol
+++ packages/ppf-contracts/contracts/IFeed.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
 
 interface IFeed {
+    function ratePrecision() external pure returns (uint256);
     function get(address base, address quote) external view returns (uint128 xrt, uint64 when);
 }
